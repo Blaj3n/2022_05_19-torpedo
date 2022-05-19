0f61b251364705f0b1e9567d2d22dcd1b6b709d1
Blaj3n <ulrichbence15@freemail.hu>
2022-05-19 12:28:41 +0200

    vonalak legenerálása

--- Torpedo/src/torpedo/Torpedo.java
+++ Torpedo/src/torpedo/Torpedo.java
@@ -3,17 +3,23 @@ package torpedo;
 public class Torpedo {
 
     public static void main(String[] args) {
-        
+        general();
     }
-    public static void general(){
-        
+
+    public static void general() {
+        String[] vonalak;
+        for (int i = 0; i < 7; i++) {
+            System.out.print("_");
+        }
+
     }
-    public static void hajo_helyzet(){
-        
+
+    public static void hajo_helyzet() {
+
     }
-    public static void szambeker(){
-        
+
+    public static void szambeker() {
+
     }
-    
-    
+
 }
