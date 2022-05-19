914241f2ecb03c104f427c21f5ee092f177195d8
Blaj3n <ulrichbence15@freemail.hu>
2022-05-19 12:19:07 +0200

    alapállapot

---
+++ Torpedo/src/torpedo/Torpedo.java
@@ -0,0 +1,19 @@
+package torpedo;
+
+public class Torpedo {
+
+    public static void main(String[] args) {
+        
+    }
+    public static void general(){
+        
+    }
+    public static void hajo_helyzet(){
+        
+    }
+    public static void szambeker(){
+        
+    }
+    
+    
+}
